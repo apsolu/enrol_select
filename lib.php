214291ac1afb65f3205365593a1bd9906e441b6e
Julien Boulen <julien.boulen@univ-rennes2.fr>
2020-01-27 12:47:06 +0100

    fix(notification): utilise l'adresse no-reply plutôt que l'adresse de l'administrateur de la plateforme

--- lib.php
+++ lib.php
@@ -582,7 +582,7 @@ class enrol_select_plugin extends enrol_plugin {
             $eventdata->courseid = $course->id;
             $eventdata->component = 'enrol_select';
             $eventdata->name = 'select_notification';
-            $eventdata->userfrom = get_admin();
+            $eventdata->userfrom = core_user::get_noreply_user();
             $eventdata->userto = $DB->get_record('user', array('id' => $user->userid));
             $eventdata->subject = get_string('enrolcoursesubject', 'enrol_select', $course);
             $eventdata->fullmessage = get_string('message_promote', 'enrol_select');
