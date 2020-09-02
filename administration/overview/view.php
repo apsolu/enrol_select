113010a25c728307d8635688a83e6457769b27ee
Julien Boulen <julien.boulen@univ-rennes2.fr>
2020-09-02 17:55:17 +0200

    fix(admin): ajoute des filtres sur la vue d'ensemble des inscriptions

--- administration/overview/view.php
+++ administration/overview/view.php
@@ -24,14 +24,30 @@ defined('MOODLE_INTERNAL') || die;
 
 // Fichier chargé automatiquement pour les administrateurs, mais pas pour les gestionnaires visiblement.
 require_once($CFG->dirroot.'/enrol/select/lib.php');
+require_once($CFG->dirroot.'/enrol/select/locallib.php');
+require_once($CFG->dirroot.'/enrol/select/administration/overview/view_filter_form.php');
 
 $PAGE->requires->js_call_amd('enrol_select/administration_overview', 'initialise');
+
+$sql = "SELECT u.*".
+    " FROM {user} u".
+    " JOIN {role_assignments} ra ON u.id = ra.userid AND ra.roleid = 3". // Teacher.
+    " JOIN {context} ctx ON ctx.id = ra.contextid AND ctx.contextlevel = 50". // Course context.
+    " JOIN {apsolu_courses} ac ON ac.id = ctx.instanceid".
+    " ORDER BY u.lastname, u.firstname";
+$teachers = $DB->get_recordset_sql($sql);
+
+$calendars = $DB->get_records('apsolu_calendars');
+
+$mform = new apsolu_overview_filter_form(null, array($calendars, $teachers));
+$mdata = $mform->get_data();
 
 // Liste des cours.
 $sql = "SELECT c.id, c.fullname, '0' AS count_enrols, '1' AS anomalies".
     " FROM {course} c".
     " JOIN {apsolu_courses} ac ON ac.id = c.id".
     " JOIN {course_categories} cc ON cc.id = c.category".
+    " WHERE c.visible = 1".
     " ORDER BY cc.sortorder, ac.numweekday, ac.starttime";
 $courses = $DB->get_records_sql($sql);
 
@@ -43,7 +59,7 @@ $enrolments = $DB->get_records_sql($sql);
 
 // Liste des méthodes d'inscription.
 $sql = "SELECT e.id, e.name, e.courseid, e.enrolstartdate, e.enrolenddate, e.customint1, e.customint2, e.customint3 AS quota,".
-    " ac.name AS calendar, ac.enrolstartdate AS calendar_enrolstartdate, ac.enrolenddate AS calendar_enrolenddate".
+    " ac.id AS calendarid, ac.name AS calendar, ac.enrolstartdate AS calendar_enrolstartdate, ac.enrolenddate AS calendar_enrolenddate".
     " FROM {enrol} e".
     " LEFT JOIN {apsolu_calendars} ac ON e.customchar1 = ac.id".
     " WHERE e.enrol = 'select'".
@@ -59,6 +75,7 @@ foreach ($enrols as $enrol) {
 
     if (isset($courses[$enrol->courseid]->enrols) === false) {
         $courses[$enrol->courseid]->enrols = array();
+        $courses[$enrol->courseid]->count_enrols = 0;
         $courses[$enrol->courseid]->anomalies = 0;
     }
 
@@ -90,9 +107,76 @@ foreach ($enrols as $enrol) {
     $courses[$enrol->courseid]->count_enrols++;
 }
 
+$teachers = UniversiteRennes2\Apsolu\get_activities_teachers();
+
 $data = new stdClass();
 $data->wwwroot = $CFG->wwwroot;
-$data->courses = array_values($courses);
-$data->count_courses = count($courses);
+$data->courses = array();
+$data->count_courses = 0;
+$data->form = $mform->render();
+$data->filters = isset($mdata->course);
+
+// Filtre les cours.
+foreach ($courses as $course) {
+    if ($data->filters === false) {
+        // Aucun filtre n'a été sélectionné.
+        $data->courses[] = $course;
+        $data->count_courses++;
+        continue;
+    }
+
+    // Filtre par nom de créneau horaire.
+    if (empty($mdata->course) === false && stripos($course->fullname, $mdata->course) === false) {
+        // Le filtre ne correspond pas au nom du cours.
+        continue;
+    }
+
+    // Filtre par enseignant.
+    if (empty($mdata->teacherid) === false && isset($teachers[$course->id][$mdata->teacherid]) === false) {
+        // Le filtre ne correspond pas à l'enseignant sélectionné.
+        continue;
+    }
+
+    if (empty($course->count_enrols) === true) {
+        // Le créneau n'a pas de méthodes d'inscription. On le garde.
+        $data->courses[] = $course;
+        $data->count_courses++;
+        continue;
+    }
+
+    foreach ($course->enrols as $id => $enrol) {
+        // Filtre par calendrier.
+        if (empty($mdata->calendarid) === false && $enrol->calendarid != $mdata->calendarid) {
+            // Le filtre ne correspond pas au calendrier sélectionné.
+            unset($course->enrols[$id]);
+            $course->count_enrols--;
+            continue;
+        }
+
+        // Filtre par date de début d'inscription.
+        if (empty($mdata->enrolstartdate) === false && strftime('%F', $enrol->enrolstartdate) != strftime('%F', $mdata->enrolstartdate)) {
+            // Le filtre ne correspond pas à la date de début d'inscription sélectionné.
+            unset($course->enrols[$id]);
+            $course->count_enrols--;
+            continue;
+        }
+
+        // Filtre par date de fin d'inscription.
+        if (empty($mdata->enrolenddate) === false && strftime('%F', $enrol->enrolenddate) != strftime('%F', $mdata->enrolenddate)) {
+            // Le filtre ne correspond pas à la date de fin d'inscription sélectionné.
+            unset($course->enrols[$id]);
+            $course->count_enrols--;
+            continue;
+        }
+    }
+
+    if (empty($course->count_enrols) === false) {
+        // Le créneau a toujours au moins une méthode d'inscription. On le garde.
+        $course->enrols = array_values($course->enrols);
+        $data->courses[] = $course;
+        $data->count_courses++;
+        continue;
+    }
+}
 
 echo $OUTPUT->render_from_template('enrol_select/administration_overview', $data);
---
+++ administration/overview/view_filter_form.php
@@ -0,0 +1,88 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Classe pour le formulaire permettant de filtrer la vue d'ensemble des inscriptions.
+ *
+ * @package    enrol_select
+ * @copyright  2020 Université Rennes 2 <dsi-contact@univ-rennes2.fr>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir.'/formslib.php');
+
+/**
+ * Classe pour le formulaire permettant de filtrer la vue d'ensemble des inscriptions.
+ *
+ * @package    enrol_select
+ * @copyright  2020 Université Rennes 2 <dsi-contact@univ-rennes2.fr>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class apsolu_overview_filter_form extends moodleform {
+    /**
+     * Définit les champs du formulaire.
+     *
+     * @return void
+     */
+    public function definition() {
+        global $CFG, $DB;
+
+        $mform = $this->_form;
+
+        list($calendars, $teachers) = $this->_customdata;
+
+        // Champ "créneaux horaires".
+        $mform->addElement('text', 'course', get_string('course', 'local_apsolu'));
+        $mform->setType('course', PARAM_TEXT);
+
+        // Champ "calendriers".
+        $options = array(0 => get_string('choosedots'));
+        foreach ($calendars as $calendar) {
+            $options[$calendar->id] = $calendar->name;
+        }
+        $mform->addElement('select', 'calendarid', get_string('calendar', 'local_apsolu'), $options);
+        $mform->setType('calendarid', PARAM_INT);
+
+        // Champ "date d'ouverture des inscriptions".
+        $mform->addElement('date_selector', 'enrolstartdate', get_string('enrolstartdate', 'local_apsolu'), array('optional'  => true));
+        $mform->setType('enrolstartdate', PARAM_INT);
+
+        // Champ "date de fermeture des inscriptions".
+        $mform->addElement('date_selector', 'enrolenddate', get_string('enrolenddate', 'local_apsolu'), array('optional'  => true));
+        $mform->setType('enrolenddate', PARAM_INT);
+
+        // Champ "enseignants".
+        $options = array(0 => get_string('choosedots'));
+        foreach ($teachers as $teacher) {
+            $options[$teacher->id] = fullname($teacher);
+        }
+        $select = $mform->addElement('select', 'teacherid', get_string('teacher', 'local_apsolu'), $options);
+        $mform->setType('teacherid', PARAM_INT);
+
+        // Submit buttons.
+        $a = new stdClass();
+        $a->href = $CFG->wwwroot.'/enrol/select/administration.php?tab=colleges';
+        $a->class = 'btn btn-default';
+
+        $buttonarray[] = &$mform->createElement('submit', 'submitbutton', get_string('filter', 'local_apsolu'));
+        $mform->addGroup($buttonarray, 'buttonar', '', array(' '), false);
+
+        $mform->addElement('hidden', 'tab', 'overview');
+        $mform->setType('tab', PARAM_ALPHA);
+    }
+}
