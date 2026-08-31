3cb8eac6d44230a565af2de024b2c7c1eea7c092
Julien Boulen <julien.boulen@univ-rennes2.fr>
2026-08-31 14:16:09 +0200

    feat(core): publie la version 5.2.2

--- version.php
+++ version.php
@@ -25,12 +25,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 // The current plugin version (Date: YYYYMMDDXX).
-$plugin->version = 2026061000;
+$plugin->version = 2026083100;
 
-$plugin->release = '5.0.8';
+$plugin->release = '5.2.2';
 
-// Requires Moodle 5.0.0.
-$plugin->requires = 2025041400;
+// Requires Moodle 5.2.0.
+$plugin->requires = 2026042000;
 
 // Full name of the plugin (used for diagnostics).
 $plugin->component = 'enrol_select';
@@ -38,10 +38,10 @@ $plugin->component = 'enrol_select';
 // The plugin is a stable version.
 $plugin->maturity = MATURITY_STABLE;
 
-// Support Moodle from 5.0.0 to 5.0.x.
-$plugin->supported = [500, 500];
+// Support Moodle from 5.2.0 to 5.2.x.
+$plugin->supported = [502, 502];
 
-// Dependencies on another plugin.
+// This is a list of plugins, this plugin depends on (and their versions).
 $plugin->dependencies = [
-    'local_apsolu' => '2026061000',
+    'local_apsolu' => '2026083100',
 ];
